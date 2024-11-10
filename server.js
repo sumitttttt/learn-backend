dfd96dc84efed69a62a68b9ff42dba1218e52e7a
Drake <sumitdrak@gmail.com>
2024-11-11 01:08:51 +0530

    express and middleware begins

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.use(function(req,res,next){
+    next()
+})
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
