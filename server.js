6d41e1d55af167fbb7c77207c065c1b538807ab8
Drake <sumitdrak@gmail.com>
2024-11-12 05:27:57 +0530

    Revert "fresh start"
    This reverts commit 0eed10002d29da8e23aa2287bd89339205c1cc70.

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const path = require('path')
+
 const app = express()
+
+app.use(function(req,res,next){
+    next()
+})
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -9,6 +14,18 @@ app.set("view engine","ejs")
 
 app.get('/', function (req, res) {
     res.render("index")
+})
+
+app.get('/profile', function (req, res) {
+    res.render("index")
+})
+
+app.get('/profile/:username', function (req, res) {
+    res.send(req.params.username)
+})
+
+app.get('/profile/:username/:age', function (req, res) {
+    res.send(req.params)
 })
 
 app.listen(3000)
