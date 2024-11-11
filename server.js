765b7aa75c3f5437866f2f5a2472e1b90267a11d
Drake <sumitdrak@gmail.com>
2024-11-12 02:21:01 +0530

    express req body encoded

--- server.js
+++ server.js
@@ -4,8 +4,22 @@ const app = express()
 app.use(function(req,res,next){
     next()
 })
+
+app.use(express.urlencoded())
+app.use(express.json())
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+    res.send('Hello World')
+})
+
+app.post('/', function (req, res) {
+    console.log(req.body)
+    res.send('Hello World')
+})
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
 })
 
 app.listen(3000)
