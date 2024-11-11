1ab149e23e55a78de702cdeac6117ed31625a32e
Drake <sumitdrak@gmail.com>
2024-11-12 03:22:27 +0530

    add ejs folder structure and dynamic routing

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 
 app.use(function(req,res,next){
     next()
 })
 
-app.use(express.urlencoded())
 app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,"public")))
+app.set("view engine","ejs")
 
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.render("index")
 })
 
-app.post('/', function (req, res) {
-    console.log(req.body)
-    res.send('Hello World')
+app.get('/profile', function (req, res) {
+    res.render("index")
 })
 
-app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.status(500).send('Something broke!')
+app.get('/profile/:username', function (req, res) {
+    res.send(req.params.username)
+})
+
+app.get('/profile/:username/:age', function (req, res) {
+    res.send(req.params)
 })
 
 app.listen(3000)
