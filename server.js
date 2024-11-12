7899a6c946350b2dc3fdd4e2a746832b4baf0696
Drake <sumitdrak@gmail.com>
2024-11-13 02:27:15 +0530

    jwt done

--- server.js
+++ server.js
@@ -3,24 +3,23 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
 
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 app.get('/', function (req, res) {
+    let token = jwt.sign({email:"sumit@gmail.com"},"secret")
+    res.cookie("token",token)
+    res.send(token)
+})
 
-    // bcrypt.genSalt(10, function(err, salt) {
-    //     bcrypt.hash("sumit", salt, function(err, hash) {
-    //         res.send(hash)
-    //     });
-    // });
-    
-    bcrypt.compare("sumit", hash, function(err, result) {
-        // result == true
-            res.send(result)
-    });
-
+app.get('/read', function (req, res) {
+    let data = jwt.verify(req.cookies.token,"secret")
+    res.send(data)
+    // res.send(req.cookies.token)
 })
 
 app.listen(3000)
