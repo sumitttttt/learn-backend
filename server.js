aca18a48cdb4fd71f13729ac97c11258cb0a2437
Drake <sumitdrak@gmail.com>
2024-11-13 01:48:40 +0530

    a&a begins

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(express.static(path.join(__dirname,"public")))
-app.set("view engine","ejs")
 
 app.get('/', function (req, res) {
-    res.render("index")
+    res.send("Done")
 })
 
 app.listen(3000)
