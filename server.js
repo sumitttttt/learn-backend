cb9f79bf3cc3a8ebe4703d224a702eca4bcdc2c9
Drake <sumitdrak@gmail.com>
2024-11-12 17:40:43 +0530

    done all http methods using get

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const userModel = require('./usermodel')
 const path = require('path')
 const app = express()
 
@@ -9,6 +10,34 @@ app.set("view engine","ejs")
 
 app.get('/', function (req, res) {
     res.render("index")
+})
+
+app.get('/create', async function (req, res) {
+    let opt= await userModel.create({
+        name:"Sumit",
+        username:"Sumit",
+        email:"sumit@gmail.com"
+    })
+
+    res.send(opt)
+})
+
+app.get('/update', async function (req, res) {
+    let updatedOpt = await userModel.findOneAndUpdate({username:"Sumit"},{name:"updatedSumit"},{new:true})
+
+    res.send(updatedOpt)
+})
+
+app.get('/read', async function (req, res) {
+    let allUsers= await userModel.find({username:"Sumit"});
+
+    res.send(allUsers)
+})
+
+app.get('/delete', async function (req, res) {
+    let allUsers= await userModel.findOneAndDelete({username:"Sumit"});
+
+    res.send(allUsers)
 })
 
 app.listen(3000)
