507091e6d3cd2d6932f478e26e232985b6fda294
Drake <sumitdrak@gmail.com>
2024-11-12 05:56:30 +0530

    again fresh

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 const express = require('express')
 const path = require('path')
-
 const app = express()
-
-app.use(function(req,res,next){
-    next()
-})
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -14,18 +9,6 @@ app.set("view engine","ejs")
 
 app.get('/', function (req, res) {
     res.render("index")
-})
-
-app.get('/profile', function (req, res) {
-    res.render("index")
-})
-
-app.get('/profile/:username', function (req, res) {
-    res.send(req.params.username)
-})
-
-app.get('/profile/:username/:age', function (req, res) {
-    res.send(req.params)
 })
 
 app.listen(3000)
