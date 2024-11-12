0b31d8ca1c487139ad9529423b998a5127206033
Drake <sumitdrak@gmail.com>
2024-11-13 02:13:02 +0530

    bcrypt done

--- server.js
+++ server.js
@@ -2,18 +2,25 @@ const cookieParser = require('cookie-parser')
 const express = require('express')
 const path = require('path')
 const app = express()
+const bcrypt = require('bcrypt')
 
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 app.get('/', function (req, res) {
-    res.cookie("special","sandt")
-    res.send("Done")
-})
 
-app.get('/read', function (req, res) {
-    res.send(req.cookies)
+    // bcrypt.genSalt(10, function(err, salt) {
+    //     bcrypt.hash("sumit", salt, function(err, hash) {
+    //         res.send(hash)
+    //     });
+    // });
+    
+    bcrypt.compare("sumit", hash, function(err, result) {
+        // result == true
+            res.send(result)
+    });
+
 })
 
 app.listen(3000)
