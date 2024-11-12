761c85a19bfc7d954717d6b632a5e376818e9c7e
Drake <sumitdrak@gmail.com>
2024-11-13 01:59:52 +0530

    cookie done

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
+const cookieParser = require('cookie-parser')
 const express = require('express')
 const path = require('path')
 const app = express()
 
+app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 app.get('/', function (req, res) {
+    res.cookie("special","sandt")
     res.send("Done")
+})
+
+app.get('/read', function (req, res) {
+    res.send(req.cookies)
 })
 
 app.listen(3000)
