1029443ea1c2ddb0f9826d2dbd22684d7e1c8a6d
Drake <sumitdrak@gmail.com>
2024-11-12 10:54:18 +0530

    mongoose initialized

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const path = require('path')
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(express.static(path.join(__dirname,"public")))
 app.set("view engine","ejs")
 
 app.get('/', function (req, res) {
