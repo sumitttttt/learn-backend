7e97c2bbe85815d5654cc0ba6430b8b5be7cf5f0
Drake <sumitdrak@gmail.com>
2024-11-12 20:14:33 +0530

    add edit functionality

--- server.js
+++ server.js
@@ -35,4 +35,21 @@ app.get('/delete/:id', async function (req, res) {
     res.redirect("/read")
 })
 
+app.get('/edit/:id', async function (req, res) {
+    let user = await userModel.findOne({_id:req.params.id})
+    res.render("edit",{user:user})
+})
+
+app.post('/update/:id', async function (req, res) {
+    let {name,email,image}=req.body
+    let user = await userModel.findOneAndUpdate({_id:req.params.id},{
+        name,
+        email,
+        image
+    },
+    {new:true})
+
+    res.redirect("/read")
+})
+
 app.listen(3000)
