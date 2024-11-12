344d68c7c1ce5106035d2e88c15b22edb349f9d0
Drake <sumitdrak@gmail.com>
2024-11-12 08:50:57 +0530

    first ui ejs project done

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
 const app = express()
 
 app.use(express.json())
@@ -8,7 +9,21 @@ app.use(express.static(path.join(__dirname,"public")))
 app.set("view engine","ejs")
 
 app.get('/', function (req, res) {
-    res.render("index")
+    fs.readdir("./files",function(err,files){
+            res.render("index",{files:files})
+        })
+    })
+    
+app.get('/file/:filename', function (req, res) {
+    fs.readFile(`./files/${req.params.filename}`,"utf-8",function(err,data){
+            res.render("show",{filename:req.params.filename,filedata:data})
+        })
+    })
+
+app.post('/create', function (req, res) {
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,function(err){
+        res.redirect("/")
+    })
 })
 
 app.listen(3000)
