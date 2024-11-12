d1580f1d2a08ff79abcf9f50851cc929cf53d893
Drake <sumitdrak@gmail.com>
2024-11-12 19:35:36 +0530

    whole mongo project done, need to edit functionality

--- model/usermodel.js
+++ model/usermodel.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose')
 mongoose.connect(`mongodb://127.0.0.1:27017/mongopractice`)
 
 const userSchema = mongoose.Schema({
-    name:String,
-    username:String,
-    email:String
+    image:String,
+    email:String,
+    name:String
 })
 
 module.exports = mongoose.model("user",userSchema)
--- server.js
+++ server.js
@@ -1,43 +1,38 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const userModel = require('./usermodel')
+const userModel = require('./model/usermodel')
 const path = require('path')
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,"public")))
 app.set("view engine","ejs")
 
 app.get('/', function (req, res) {
     res.render("index")
 })
 
-app.get('/create', async function (req, res) {
-    let opt= await userModel.create({
-        name:"Sumit",
-        username:"Sumit",
-        email:"sumit@gmail.com"
+app.get('/read', async function (req, res) {
+    let users = await userModel.find()
+    res.render("read",{users:users})
+})
+
+app.post('/create', async function (req, res) {
+    let {name,email,image}=req.body
+
+    let createdUser= await userModel.create({
+        name:name,
+        email,
+        image:image
     })
 
-    res.send(opt)
+    res.redirect("read")
 })
 
-app.get('/update', async function (req, res) {
-    let updatedOpt = await userModel.findOneAndUpdate({username:"Sumit"},{name:"updatedSumit"},{new:true})
-
-    res.send(updatedOpt)
-})
-
-app.get('/read', async function (req, res) {
-    let allUsers= await userModel.find({username:"Sumit"});
-
-    res.send(allUsers)
-})
-
-app.get('/delete', async function (req, res) {
-    let allUsers= await userModel.findOneAndDelete({username:"Sumit"});
-
-    res.send(allUsers)
+app.get('/delete/:id', async function (req, res) {
+    let users = await userModel.findOneAndDelete({_id:req.params.id})
+    res.redirect("/read")
 })
 
 app.listen(3000)
